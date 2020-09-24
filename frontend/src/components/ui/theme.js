4607b593e7d67b199e7e5c3fa1c02cf49cfc22a3
Zachary Reece <zachdreece@gmail.com>
2020-09-24 00:39:34 -0500

    Filled Out Theme From Design File

--- frontend/src/components/ui/theme.js
+++ frontend/src/components/ui/theme.js
@@ -1,18 +1,67 @@
 import { createMuiTheme } from "@material-ui/core/styles"
 
+const green = "#99B898"
+const darkGreen = "#708670"
+const tan = "#FECEA8"
+const lightRed = "#FF847C"
+const red = "#E84A5F"
+const offBlack = "#2A363B"
+const grey = "#747474"
+
 const theme = createMuiTheme({
   palette: {
     primary: {
-      main: "",
+      main: green,
     },
     secondary: {
-      main: "",
+      main: darkGreen,
     },
-    common: {},
+    common: {
+      tan,
+      lightRed,
+      red,
+      offBlack,
+    },
   },
   typography: {
-    h1: {},
-    body1: {},
+    h1: {
+      fontSize: "4.5rem",
+      fontFamily: "Philosopher",
+      fontStyle: "italic",
+      fontWeight: 700,
+      color: green,
+    },
+    h2: {
+      fontFamily: "Montserrat",
+      fontSize: "3rem",
+      fontWeight: 500,
+      color: "#fff",
+    },
+    h3: {
+      fontFamily: "Montserrat",
+      fontSize: "2rem",
+      fontWeight: 300,
+      color: green,
+    },
+    h4: {
+      fontFamily: "Philosopher",
+      fontStyle: "italic",
+      fontWeight: 700,
+      fontSize: "3rem",
+      color: "#fff",
+    },
+    h5: {
+      fontFamily: "Philosopher",
+      fontSize: "2rem",
+      fontWeight: 700,
+      fontStyle: "italic",
+      color: "#fff",
+    },
+    body1: {
+      fontFamily: "Montserrat",
+      fontSize: "1.5rem",
+      color: grey,
+    },
   },
   overrides: {},
 })
