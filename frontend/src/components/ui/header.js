e718d0cc1c1c336a695d92a19a46bf07190c887d
Zachary Reece <zachdreece@gmail.com>
2020-09-25 00:14:55 -0500

    Creating A Header

--- frontend/src/components/ui/header.js
+++ frontend/src/components/ui/header.js
@@ -1,42 +1,39 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
 import React from "react"
+import AppBar from "@material-ui/core/AppBar"
+import Toolbar from "@material-ui/core/Toolbar"
+import Typography from "@material-ui/core/Typography"
+import Button from "@material-ui/core/Button"
+import IconButton from "@material-ui/core/IconButton"
+import Tabs from "@material-ui/core/Tabs"
+import Tab from "@material-ui/core/Tab"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
+import search from "../../images/search.svg"
+import cart from "../../images/cart.svg"
+import account from "../../images/account-header.svg"
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
+export default function Header() {
+  return (
+    <AppBar color="transparent" elevation={0}>
+      <Toolbar>
+        <Button>
+          <Typography variant="h1">VAR X</Typography>
+        </Button>
+        <Tabs>
+          <Tab label="Hats" />
+          <Tab label="Hoodies" />
+          <Tab label="Shirts" />
+          <Tab label="Contact Us" />
+        </Tabs>
+        <IconButton>
+          <img src={search} alt="search" />
+        </IconButton>
+        <IconButton>
+          <img src={cart} alt="cart" />
+        </IconButton>
+        <IconButton>
+          <img src={account} alt="account" />
+        </IconButton>
+      </Toolbar>
+    </AppBar>
+  )
 }
-
-Header.defaultProps = {
-  siteTitle: ``,
-}
-
-export default Header
--- frontend/src/components/ui/layout.js
+++ frontend/src/components/ui/layout.js
@@ -10,7 +10,6 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
-import "./layout.css"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -25,7 +24,7 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
+      <Header />
       <div
         style={{
           margin: `0 auto`,
@@ -34,13 +33,15 @@ const Layout = ({ children }) => {
         }}
       >
         <main>{children}</main>
-        <footer style={{
-          marginTop: `2rem`
-        }}>
+        {/* <footer
+          style={{
+            marginTop: `2rem`,
+          }}
+          >
           © {new Date().getFullYear()}, Built with
           {` `}
           <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer>
+        </footer> */}
       </div>
     </>
   )
