183c52c2b9df5f1c92287f7eb3b2be4966fd7666
Zachary Reece <zachdreece@gmail.com>
2020-09-30 00:57:43 -0500

    Customizing Material-UI Components -- Styled Header

--- frontend/src/components/ui/header.js
+++ frontend/src/components/ui/header.js
@@ -6,19 +6,40 @@ import Button from "@material-ui/core/Button"
 import IconButton from "@material-ui/core/IconButton"
 import Tabs from "@material-ui/core/Tabs"
 import Tab from "@material-ui/core/Tab"
+import { makeStyles } from "@material-ui/core/styles"
 
 import search from "../../images/search.svg"
 import cart from "../../images/cart.svg"
 import account from "../../images/account-header.svg"
 
+const useStyles = makeStyles(theme => ({
+  coloredIndicator: {
+    backgroundColor: "#fff",
+  },
+  logoText: {
+    color: theme.palette.common.offBlack,
+  },
+  tabs: {
+    marginLeft: "auto",
+    marginRight: "auto",
+  },
+}))
+
 export default function Header() {
+  const classes = useStyles()
+
   return (
     <AppBar color="transparent" elevation={0}>
       <Toolbar>
         <Button>
-          <Typography variant="h1">VAR X</Typography>
+          <Typography variant="h1">
+            <span className={classes.logoText}>VAR</span> X
+          </Typography>
         </Button>
-        <Tabs>
+        <Tabs
+          value={0}
+          classes={{ indicator: classes.coloredIndicator, root: classes.tabs }}
+        >
           <Tab label="Hats" />
           <Tab label="Hoodies" />
           <Tab label="Shirts" />
