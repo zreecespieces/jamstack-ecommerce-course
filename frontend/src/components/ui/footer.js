bfb22df9e3fe518150e3feabe5e0ff96412619e9
Zachary Reece <zachdreece@gmail.com>
2020-10-08 22:42:17 -0500

    Footer Refactor

--- frontend/src/components/ui/footer.js
+++ frontend/src/components/ui/footer.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Grid from "@material-ui/core/Grid"
 import Typography from "@material-ui/core/Typography"
+import IconButton from "@material-ui/core/IconButton"
 import { makeStyles } from "@material-ui/core/styles"
 import { Link } from "gatsby"
 
@@ -20,18 +21,22 @@ const useStyles = makeStyles(theme => ({
   linkColumn: {
     width: "20rem",
   },
-  spacer: {
-    marginTop: "2rem",
-    marginBottom: "2rem",
-  },
   linkContainer: {
     [theme.breakpoints.down("md")]: {
       marginBottom: "3rem",
     },
   },
+  icon: {
+    "&:hover": {
+      backgroundColor: "transparent",
+    },
+  },
   "@global": {
     body: {
       margin: 0,
+    },
+    a: {
+      textDecoration: "none",
     },
   },
 }))
@@ -39,86 +44,76 @@ const useStyles = makeStyles(theme => ({
 export default function Footer() {
   const classes = useStyles()
 
+  const socialMedia = [
+    { icon: facebook, alt: "facebook", link: "https://facebook.com" },
+    { icon: twitter, alt: "twitter", link: "https://twitter.com" },
+    { icon: instagram, alt: "instagram", link: "https://instagram.com" },
+  ]
+
+  const routes = {
+    "Contact Us": [
+      { label: "(555) 555-5555", href: "tel:(555) 555-5555" },
+      { label: "zachary@var-x.com", href: "mailto:zachary@var-x.com" },
+    ],
+    "Customer Service": [
+      { label: "Contact Us", link: "/contact" },
+      { label: "My Account", link: "/account" },
+    ],
+    Information: [
+      { label: "Privacy Policy", link: "/privacy-policy" },
+      { label: "Terms and Conditions", link: "/terms-conditions" },
+    ],
+  }
+
   return (
     <footer className={classes.footer}>
       <Grid container justify="space-between">
         {/* Links */}
         <Grid item classes={{ root: classes.linkContainer }}>
           <Grid container>
-            <Grid
-              item
-              container
-              direction="column"
-              classes={{ root: classes.linkColumn }}
-            >
-              <Grid item>
-                <Typography variant="h5">Contact Us</Typography>
+            {Object.keys(routes).map(category => (
+              <Grid
+                item
+                key={category}
+                container
+                direction="column"
+                classes={{ root: classes.linkColumn }}
+              >
+                <Grid item>
+                  <Typography variant="h5">{category}</Typography>
+                </Grid>
+                {routes[category].map(route => (
+                  <Grid item key={route.label}>
+                    <Typography
+                      component={route.link ? Link : "a"}
+                      to={route.link ? route.link : undefined}
+                      href={route.href ? route.href : undefined}
+                      variant="body1"
+                      classes={{ body1: classes.link }}
+                    >
+                      {route.label}
+                    </Typography>
+                  </Grid>
+                ))}
               </Grid>
-              <Grid item>
-                <Typography variant="body1" classes={{ body1: classes.link }}>
-                  (555) 555-5555
-                </Typography>
-              </Grid>
-              <Grid item>
-                <Typography variant="body1" classes={{ body1: classes.link }}>
-                  zachary@var-x.com
-                </Typography>
-              </Grid>
-            </Grid>
-            <Grid
-              item
-              container
-              direction="column"
-              classes={{ root: classes.linkColumn }}
-            >
-              <Grid item>
-                <Typography variant="h5">Customer Service</Typography>
-              </Grid>
-              <Grid item>
-                <Typography variant="body1" classes={{ body1: classes.link }}>
-                  Contact Us
-                </Typography>
-              </Grid>
-              <Grid item>
-                <Typography variant="body1" classes={{ body1: classes.link }}>
-                  My Account
-                </Typography>
-              </Grid>
-            </Grid>
-            <Grid
-              item
-              container
-              direction="column"
-              classes={{ root: classes.linkColumn }}
-            >
-              <Grid item>
-                <Typography variant="h5">Information</Typography>
-              </Grid>
-              <Grid item>
-                <Typography variant="body1" classes={{ body1: classes.link }}>
-                  Privacy Policy
-                </Typography>
-              </Grid>
-              <Grid item>
-                <Typography variant="body1" classes={{ body1: classes.link }}>
-                  Terms & Conditions
-                </Typography>
-              </Grid>
-            </Grid>
+            ))}
           </Grid>
         </Grid>
         {/* Social Media Icons */}
         <Grid item>
           <Grid container direction="column" alignItems="center">
-            <Grid item>
-              <img src={facebook} alt="facebook" />
-            </Grid>
-            <Grid item classes={{ root: classes.spacer }}>
-              <img src={twitter} alt="twitter" />
-            </Grid>
-            <Grid item>
-              <img src={instagram} alt="instagram" />
-            </Grid>
+            {socialMedia.map(platform => (
+              <Grid item key={platform.alt}>
+                <IconButton
+                  classes={{ root: classes.icon }}
+                  component="a"
+                  disableRipple
+                  href={platform.link}
+                >
+                  <img src={platform.icon} alt={platform.alt} />
+                </IconButton>
+              </Grid>
+            ))}
           </Grid>
         </Grid>
       </Grid>
