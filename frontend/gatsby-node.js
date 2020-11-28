7ae6f3e9f7b17c52b6ea9040c7ab39cad2d9c67c
Zachary Reece <zachdreece@gmail.com>
2020-11-28 00:44:51 -0600

    Storing Recently Viewed Products

--- frontend/gatsby-node.js
+++ frontend/gatsby-node.js
@@ -79,6 +79,7 @@ exports.createPages = async ({ graphql, actions }) => {
         category: product.node.category.name,
         description: product.node.description,
         variants: product.node.variants,
+        product: product,
       },
     })
   })
--- frontend/src/templates/ProductDetail.js
+++ frontend/src/templates/ProductDetail.js
@@ -6,7 +6,7 @@ import ProductImages from "../components/product-detail/ProductImages"
 import ProductInfo from "../components/product-detail/ProductInfo"
 
 export default function ProductDetail({
-  pageContext: { name, id, category, description, variants },
+  pageContext: { name, id, category, description, variants, product },
 }) {
   const [selectedVariant, setSelectedVariant] = useState(0)
   const [selectedImage, setSelectedImage] = useState(0)
@@ -17,6 +17,27 @@ export default function ProductDetail({
     const styledVariant = variants.filter(
       variant => variant.style === params.get("style")
     )[0]
+
+    var recentlyViewed = JSON.parse(
+      window.localStorage.getItem("recentlyViewed")
+    )
+
+    if (recentlyViewed) {
+      if (recentlyViewed.length === 10) {
+        recentlyViewed.shift()
+      }
+
+      if (!recentlyViewed.some(product => product.node.name === name)) {
+        recentlyViewed.push(product)
+      }
+    } else {
+      recentlyViewed = [product]
+    }
+
+    window.localStorage.setItem(
+      "recentlyViewed",
+      JSON.stringify(recentlyViewed)
+    )
 
     setSelectedVariant(variants.indexOf(styledVariant))
   }, [])
