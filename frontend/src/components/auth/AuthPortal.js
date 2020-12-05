572b891eae89c67641bf373b26bc77347e2b62ed
Zachary Reece <zachdreece@gmail.com>
2020-12-04 23:12:15 -0600

    Building AuthPortal

---
+++ frontend/src/components/auth/AuthPortal.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react"
+import Grid from "@material-ui/core/Grid"
+import Typography from "@material-ui/core/Typography"
+import Paper from "@material-ui/core/Paper"
+import { makeStyles } from "@material-ui/core/styles"
+
+const useStyles = makeStyles(theme => ({
+  paper: {
+    border: `2rem solid ${theme.palette.secondary.main}`,
+    width: "50rem",
+    height: "40rem",
+    borderRadius: 0,
+  },
+  inner: {
+    height: "40rem",
+    width: "100%",
+    border: `2rem solid ${theme.palette.primary.main}`,
+  },
+  container: {
+    marginBottom: "8rem",
+  },
+}))
+
+export default function AuthPortal() {
+  const classes = useStyles()
+  const [selectedStep, setSelectedStep] = useState(0)
+
+  const steps = [{ component: Login, label: "Login" }]
+
+  return (
+    <Grid container justify="center" classes={{ root: classes.container }}>
+      <Grid item>
+        <Paper elevation={6} classes={{ root: classes.paper }}>
+          <Grid
+            container
+            direction="column"
+            alignItems="center"
+            classes={{ root: classes.inner }}
+          >
+            {steps.map((Step, i) =>
+              selectedStep === i ? (
+                <Step.component
+                  setSelectedStep={setSelectedStep}
+                  steps={steps}
+                  key={Step.label}
+                />
+              ) : null
+            )}
+          </Grid>
+        </Paper>
+      </Grid>
+    </Grid>
+  )
+}
---
+++ frontend/src/pages/account.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+import Layout from "../components/ui/layout"
+import AuthPortal from "../components/auth/AuthPortal"
+
+export default function Account() {
+  return (
+    <Layout>
+      <AuthPortal />
+    </Layout>
+  )
+}
