1f501a20e5f0b40fe678e8cefa040775317f6bd5
Zachary Reece <zachdreece@gmail.com>
2020-12-30 23:25:49 -0600

    Facebook Auth Config

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,9 +1,10 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  url: env("", "https://2d65a7e595af.ngrok.io"),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', 'c423fcb4b1d5ec19947db8a7a7bb9104'),
+      secret: env("ADMIN_JWT_SECRET", "c423fcb4b1d5ec19947db8a7a7bb9104"),
     },
   },
 });
