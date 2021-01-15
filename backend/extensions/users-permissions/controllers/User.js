923d9c82a2b9a75988877875e3a071d5fc5a14a3
Zachary Reece <zachdreece@gmail.com>
2021-01-14 22:26:06 -0600

    Custom Controllers

---
+++ backend/extensions/users-permissions/controllers/User.js
@@ -0,0 +1,36 @@
+const { sanitizeEntity } = require("strapi-utils");
+
+const sanitizeUser = (user) =>
+  sanitizeEntity(user, {
+    model: strapi.query("user", "users-permissions").model,
+  });
+
+module.exports = {
+  async setSettings(ctx) {
+    const { id, contactInfo, locations } = ctx.state.user;
+    const { details, detailSlot, location, locationSlot } = ctx.request.body;
+
+    let newInfo = [...contactInfo];
+    let newLocations = [...locations];
+
+    if (typeof details !== "undefined" && typeof detailSlot !== "undefined") {
+      newInfo[detailSlot] = details;
+    }
+
+    if (
+      typeof location !== "undefined" &&
+      typeof locationSlot !== "undefined"
+    ) {
+      newLocations[locationSlot] = location;
+    }
+
+    let newUser = await strapi.plugins["users-permissions"].services.user.edit(
+      { id },
+      { contactInfo: newInfo, locations: newLocations }
+    );
+
+    newUser = sanitizeUser(newUser);
+
+    ctx.send(newUser, 200);
+  },
+};
