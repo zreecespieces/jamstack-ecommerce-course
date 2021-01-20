0823bbde98c45d0f642099be49b27ea4ae8ddb14
Zachary Reece <zachdreece@gmail.com>
2021-01-19 22:57:06 -0600

    Cart Context Reducer

--- frontend/src/contexts/actions/cart-actions.js
+++ frontend/src/contexts/actions/cart-actions.js
@@ -1,8 +1,8 @@
 import { ADD_TO_CART, REMOVE_FROM_CART, CLEAR_CART } from "./action-types"
 
-export const addToCart = (variant, qty, name) => ({
+export const addToCart = (variant, qty, name, stock) => ({
   type: ADD_TO_CART,
-  payload: { variant, qty, name },
+  payload: { variant, qty, name, stock },
 })
 
 export const removeFromCart = (variant, qty) => ({
---
+++ frontend/src/contexts/reducers/cart-reducer.js
@@ -0,0 +1,60 @@
+import {
+  ADD_TO_CART,
+  REMOVE_FROM_CART,
+  CLEAR_CART,
+} from "../actions/action-types"
+
+export default function cartReducer(state, action) {
+  let newCart = [...state]
+
+  let existingIndex
+
+  if (action.payload) {
+    existingIndex = state.findIndex(
+      item => item.variant === action.payload.variant
+    )
+  }
+
+  const saveData = cart => {
+    localStorage.setItem("cart", JSON.stringify(cart))
+  }
+
+  switch (action.type) {
+    case ADD_TO_CART:
+      if (existingIndex !== -1) {
+        let newQty = newCart[existingIndex].qty + action.payload.qty
+
+        if (newQty > action.payload.stock) {
+          newQty = action.payload.stock
+        }
+
+        newCart[existingIndex] = { ...newCart[existingIndex], qty: newQty }
+      } else {
+        newCart.push(action.payload)
+      }
+
+      saveData(newCart)
+
+      return newCart
+    case REMOVE_FROM_CART:
+      const newQty = newCart[existingIndex].qty - action.payload.qty
+
+      if (newQty <= 0) {
+        newCart = newCart.filter(
+          item => item.variant !== action.payload.variant
+        )
+      } else {
+        newCart[existingIndex] = { ...newCart[existingIndex], qty: newQty }
+      }
+
+      saveData(newCart)
+
+      return newCart
+    case CLEAR_CART:
+      localStorage.removeItem("cart")
+
+      return []
+    default:
+      return state
+  }
+}
