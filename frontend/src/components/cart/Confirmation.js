7d614195ac9e81e4c5a327a5722a1fe342fd38d1
Zachary Reece <zachdreece@gmail.com>
2021-01-31 22:14:04 -0600

    Confirmation Fields

--- frontend/src/components/cart/Confirmation.js
+++ frontend/src/components/cart/Confirmation.js
@@ -10,14 +10,22 @@ import PhoneAdornment from "../../images/PhoneAdornment"
 import streetAdornment from "../../images/street-adornment.svg"
 import zipAdornment from "../../images/zip-adornment.svg"
 import cardAdornment from "../../images/card.svg"
-import promoAdornment from "../../images/promo-adornment.svg"
+import promoAdornment from "../../images/promo-code.svg"
 
 const useStyles = makeStyles(theme => ({
   nameWrapper: {
     height: 22,
     width: 22,
   },
-  name: {
+  emailWrapper: {
+    height: 17,
+    width: 22,
+  },
+  phoneWrapper: {
+    height: 25.122,
+    width: 25.173,
+  },
+  text: {
     fontSize: "1rem",
     color: "#fff",
   },
@@ -26,22 +34,71 @@ const useStyles = makeStyles(theme => ({
 export default function Confirmation() {
   const classes = useStyles()
 
+  const firstFields = [
+    {
+      value: "Zachary Reece",
+      adornment: (
+        <div className={classes.nameWrapper}>
+          <NameAdornment color="#fff" />
+        </div>
+      ),
+    },
+    {
+      value: "zachary@var-x.com",
+      adornment: (
+        <div className={classes.emailWrapper}>
+          <EmailAdornment color="#fff" />
+        </div>
+      ),
+    },
+    {
+      value: "(555) 555-5555",
+      adornment: (
+        <div className={classes.phoneWrapper}>
+          <PhoneAdornment />
+        </div>
+      ),
+    },
+    {
+      value: "1234 Example St",
+      adornment: <img src={streetAdornment} alt="street address" />,
+    },
+  ]
+
+  const secondFields = [
+    {
+      value: "Wichita, KS 67211",
+      adornment: <img src={zipAdornment} alt="city, state, zip code" />,
+    },
+    {
+      value: "**** **** **** 1234",
+      adornment: <img src={cardAdornment} alt="credit card" />,
+    },
+    {
+      promo: {
+        helperText: "",
+        placeholder: "Promo code",
+        startAdornment: <img src={promoAdornment} alt="promo code" />,
+      },
+    },
+  ]
+
   return (
     <Grid item container direction="column">
       <Grid item container>
         <Grid item container direction="column" xs={8}>
-          <Grid item container>
-            <Grid item xs={1}>
-              <div className={classes.nameWrapper}>
-                <NameAdornment color="#fff" />
-              </div>
+          {firstFields.map(field => (
+            <Grid item container key={field.value}>
+              <Grid item xs={1}>
+                {field.adornment}
+              </Grid>
+              <Grid item xs={11}>
+                <Typography variant="body1" classes={{ root: classes.text }}>
+                  {field.value}
+                </Typography>
+              </Grid>
             </Grid>
-            <Grid item xs={11}>
-              <Typography variant="body1" classes={{ root: classes.name }}>
-                Zachary Reece
-              </Typography>
-            </Grid>
-          </Grid>
+          ))}
         </Grid>
         <Grid item xs={4}>
           <img src={confirmationIcon} alt="confirmation" />
