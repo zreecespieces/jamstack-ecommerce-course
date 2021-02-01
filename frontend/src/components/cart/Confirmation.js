de1e0c4807df0da6af63218e62fd7a173680708a
Zachary Reece <zachdreece@gmail.com>
2021-01-31 22:36:28 -0600

    Confirmation Fields Continued

--- frontend/src/components/cart/Confirmation.js
+++ frontend/src/components/cart/Confirmation.js
@@ -1,7 +1,9 @@
-import React from "react"
+import React, { useState } from "react"
 import Grid from "@material-ui/core/Grid"
 import Typography from "@material-ui/core/Typography"
 import { makeStyles } from "@material-ui/core/styles"
+
+import Fields from "../auth/Fields"
 
 import confirmationIcon from "../../images/tag.svg"
 import NameAdornment from "../../images/NameAdornment"
@@ -29,10 +31,19 @@ const useStyles = makeStyles(theme => ({
     fontSize: "1rem",
     color: "#fff",
   },
+  card: {
+    height: 18,
+    width: 25,
+  },
+  priceLabel: {
+    fontSize: "1.5rem",
+  },
 }))
 
 export default function Confirmation() {
   const classes = useStyles()
+  const [promo, setPromo] = useState({ promo: "" })
+  const [promoError, setPromoError] = useState({})
 
   const firstFields = [
     {
@@ -72,7 +83,9 @@ export default function Confirmation() {
     },
     {
       value: "**** **** **** 1234",
-      adornment: <img src={cardAdornment} alt="credit card" />,
+      adornment: (
+        <img src={cardAdornment} alt="credit card" className={classes.card} />
+      ),
     },
     {
       promo: {
@@ -83,20 +96,41 @@ export default function Confirmation() {
     },
   ]
 
+  const prices = [
+    {
+      label: "SUBTOTAL",
+      value: 99.99,
+    },
+    {
+      label: "SHIPPING",
+      value: 9.99,
+    },
+    {
+      label: "TAX",
+      value: 9.67,
+    },
+  ]
+
+  const adornmentValue = (adornment, value) => (
+    <>
+      <Grid item xs={1}>
+        {adornment}
+      </Grid>
+      <Grid item xs={11}>
+        <Typography variant="body1" classes={{ root: classes.text }}>
+          {value}
+        </Typography>
+      </Grid>
+    </>
+  )
+
   return (
     <Grid item container direction="column">
       <Grid item container>
         <Grid item container direction="column" xs={8}>
           {firstFields.map(field => (
             <Grid item container key={field.value}>
-              <Grid item xs={1}>
-                {field.adornment}
-              </Grid>
-              <Grid item xs={11}>
-                <Typography variant="body1" classes={{ root: classes.text }}>
-                  {field.value}
-                </Typography>
-              </Grid>
+              {adornmentValue(field.adornment, field.value)}
             </Grid>
           ))}
         </Grid>
@@ -104,6 +138,34 @@ export default function Confirmation() {
           <img src={confirmationIcon} alt="confirmation" />
         </Grid>
       </Grid>
+      {secondFields.map((field, i) => (
+        <Grid item container key={i}>
+          <Grid item xs={6}>
+            {field.promo ? (
+              <Fields
+                fields={field}
+                values={promo}
+                setValues={setPromo}
+                errors={promoError}
+                setErrors={setPromoError}
+                isWhite
+              />
+            ) : (
+              adornmentValue(field.adornment, field.value)
+            )}
+          </Grid>
+          <Grid item container xs={6}>
+            <Grid item xs={6}>
+              <Typography variant="h5" classes={{ root: classes.priceLabel }}>
+                {prices[i].label}
+              </Typography>
+            </Grid>
+            <Grid item xs={6}>
+              <Typography variant="body2">{prices[i].value}</Typography>
+            </Grid>
+          </Grid>
+        </Grid>
+      ))}
     </Grid>
   )
 }
