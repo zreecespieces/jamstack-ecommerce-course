14fcf4eec47c415edfe11a4b31a4514ff2d75cb6
Zachary Reece <zachdreece@gmail.com>
2021-02-10 00:02:27 -0600

    Create Order Controller

--- backend/api/order/controllers/order.js
+++ backend/api/order/controllers/order.js
@@ -1,8 +1,39 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+const GUEST_ID = "602374335406731dcb566555";
+
+module.exports = {
+  async place(ctx) {
+    const {
+      shippingAddress,
+      billingAddress,
+      shippingInfo,
+      billingInfo,
+      shippingOption,
+      subtotal,
+      tax,
+      total,
+      items,
+    } = ctx.request.body;
+
+    let orderCustomer;
+
+    if (ctx.state.user) {
+      orderCustomer = ctx.state.user.id;
+    } else {
+      orderCustomer = GUEST_ID;
+    }
+
+    let serverTotal = 0;
+    let unavailable = [];
+
+    await Promise.all();
+
+    //only executed after everything in Promise.all has completed SUCCESSFULLY
+  },
+};
