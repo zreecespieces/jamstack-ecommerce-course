689d9ee0094d7bc7954ef05aa90a7e335caabe6f
Zachary Reece <zachdreece@gmail.com>
2021-02-18 23:37:33 -0600

    Confirm Payment

--- frontend/src/components/cart/Confirmation.js
+++ frontend/src/components/cart/Confirmation.js
@@ -8,6 +8,7 @@ import Button from "@material-ui/core/Button"
 import Chip from "@material-ui/core/Chip"
 import useMediaQuery from "@material-ui/core/useMediaQuery"
 import { makeStyles } from "@material-ui/core/styles"
+import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js"
 import { v4 as uuidv4 } from "uuid"
 
 import Fields from "../auth/Fields"
@@ -133,6 +134,8 @@ export default function Confirmation({
   setOrder,
 }) {
   const classes = useStyles()
+  const stripe = useStripe()
+  const elements = useElements()
   const matchesXS = useMediaQuery(theme => theme.breakpoints.down("xs"))
 
   const [loading, setLoading] = useState(false)
@@ -238,43 +241,77 @@ export default function Confirmation({
     </>
   )
 
-  const handleOrder = () => {
+  const handleOrder = async () => {
     setLoading(true)
 
-    axios
-      .post(
-        process.env.GATSBY_STRAPI_URL + "/orders/place",
-        {
-          shippingAddress: locationValues,
-          billingAddress: billingLocation,
-          shippingInfo: detailValues,
-          billingInfo: billingDetails,
-          shippingOption: shipping,
-          subtotal: subtotal.toFixed(2),
-          tax: tax.toFixed(2),
-          total: total.toFixed(2),
-          items: cart,
+    const idempotencyKey = uuidv4()
+
+    const cardElement = elements.getElement(CardElement)
+
+    const result = await stripe.confirmCardPayment(
+      clientSecret,
+      {
+        payment_method: {
+          card: cardElement,
+          billing_details: {
+            address: {
+              city: billingLocation.city,
+              state: billingLocation.state,
+              line1: billingLocation.street,
+            },
+            email: billingDetails.email,
+            name: billingDetails.name,
+            phone: billingDetails.phone,
+          },
         },
-        {
-          headers:
-            user.username === "Guest"
-              ? undefined
-              : { Authorization: `Bearer ${user.jwt}` },
-        }
+      },
+      { idempotencyKey }
+    )
+
+    if (result.error) {
+      console.error(result.error.message)
+      dispatchFeedback(
+        setSnackbar({ status: "error", message: result.error.message })
       )
-      .then(response => {
-        setLoading(false)
+      setLoading(false)
+    } else if (result.paymentIntent.status === "succeeded") {
+      console.log("PAYMENT SUCCESSFUL")
+    }
 
-        dispatchCart(clearCart())
-
-        setOrder(response.data.order)
-
-        setSelectedStep(selectedStep + 1)
-      })
-      .catch(error => {
-        setLoading(false)
-        console.error(error)
-      })
+    // axios
+    //   .post(
+    //     process.env.GATSBY_STRAPI_URL + "/orders/finalize",
+    //     {
+    //       shippingAddress: locationValues,
+    //       billingAddress: billingLocation,
+    //       shippingInfo: detailValues,
+    //       billingInfo: billingDetails,
+    //       shippingOption: shipping,
+    //       subtotal: subtotal.toFixed(2),
+    //       tax: tax.toFixed(2),
+    //       total: total.toFixed(2),
+    //       items: cart,
+    //     },
+    //     {
+    //       headers:
+    //         user.username === "Guest"
+    //           ? undefined
+    //           : { Authorization: `Bearer ${user.jwt}` },
+    //     }
+    //   )
+    //   .then(response => {
+    //     setLoading(false)
+    //
+    //     dispatchCart(clearCart())
+    //
+    //     setOrder(response.data.order)
+    //
+    //     setSelectedStep(selectedStep + 1)
+    //   })
+    //   .catch(error => {
+    //     setLoading(false)
+    //     console.error(error)
+    //   })
   }
 
   useEffect(() => {
