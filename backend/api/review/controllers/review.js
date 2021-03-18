8a14579fc39ce7cd11f862776bde207de74245c2
Zachary Reece <zachdreece@gmail.com>
2021-03-17 22:49:53 -0500

    Review Controllers

--- backend/api/review/controllers/review.js
+++ backend/api/review/controllers/review.js
@@ -1,8 +1,65 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+const { parseMultipartData, sanitizeEntity } = require("strapi-utils");
+
+module.exports = {
+  async create(ctx) {
+    let entity;
+    if (ctx.is("multipart")) {
+      const { data, files } = parseMultipartData(ctx);
+      data.user = ctx.state.user.id;
+      entity = await strapi.services.review.create(data, { files });
+    } else {
+      ctx.request.body.user = ctx.state.user.id;
+      entity = await strapi.services.review.create(ctx.request.body);
+    }
+    return sanitizeEntity(entity, { model: strapi.models.review });
+  },
+
+  async update(ctx) {
+    const { id } = ctx.params;
+
+    let entity;
+
+    const [review] = await strapi.services.review.find({
+      id,
+      user: ctx.state.user.id,
+    });
+
+    if (!review) {
+      return ctx.unauthorized(`You can't update this entry`);
+    }
+
+    if (ctx.is("multipart")) {
+      const { data, files } = parseMultipartData(ctx);
+      entity = await strapi.services.review.update({ id }, data, {
+        files,
+      });
+    } else {
+      entity = await strapi.services.review.update({ id }, ctx.request.body);
+    }
+
+    return sanitizeEntity(entity, { model: strapi.models.review });
+  },
+
+  async delete(ctx) {
+    const { id } = ctx.params;
+
+    const [review] = await strapi.services.review.find({
+      id,
+      user: ctx.state.user.id,
+    });
+
+    if (!review) {
+      return ctx.unauthorized(`You can't update this entry`);
+    }
+
+    const entity = await strapi.services.review.delete({ id });
+    return sanitizeEntity(entity, { model: strapi.models.review });
+  },
+};
