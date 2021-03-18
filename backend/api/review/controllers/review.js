afd77a59d1b6d46364149628ed825488d720ec0f
Zachary Reece <zachdreece@gmail.com>
2021-03-17 23:11:33 -0500

    Product Average Rating

--- backend/api/review/controllers/review.js
+++ backend/api/review/controllers/review.js
@@ -18,6 +18,8 @@ module.exports = {
       ctx.request.body.user = ctx.state.user.id;
       entity = await strapi.services.review.create(ctx.request.body);
     }
+
+    await strapi.services.review.average(entity.product.id);
     return sanitizeEntity(entity, { model: strapi.models.review });
   },
 
@@ -44,6 +46,7 @@ module.exports = {
       entity = await strapi.services.review.update({ id }, ctx.request.body);
     }
 
+    await strapi.services.review.average(entity.product.id);
     return sanitizeEntity(entity, { model: strapi.models.review });
   },
 
@@ -60,6 +63,7 @@ module.exports = {
     }
 
     const entity = await strapi.services.review.delete({ id });
+    await strapi.services.review.average(entity.product.id);
     return sanitizeEntity(entity, { model: strapi.models.review });
   },
 };
--- backend/api/review/services/review.js
+++ backend/api/review/services/review.js
@@ -1,8 +1,24 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/services.html#core-services)
  * to customize this service
  */
 
-module.exports = {};
+module.exports = {
+  async average(id) {
+    const product = await strapi.services.product.findOne({ id });
+
+    const total = product.reviews.reduce(
+      (total, review) => total + review.rating,
+      0
+    );
+
+    const average = total / product.reviews.length;
+
+    await strapi.services.product.update(
+      { id },
+      { rating: Math.round(average * 2) / 2 }
+    );
+  },
+};
