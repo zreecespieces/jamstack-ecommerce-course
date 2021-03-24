73ab00b644e1bd7ed767131c75673493d8ceb096
Zachary Reece <zachdreece@gmail.com>
2021-03-24 00:47:29 -0500

    Paginate Reviews

--- frontend/src/components/product-detail/ProductReviews.js
+++ frontend/src/components/product-detail/ProductReviews.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect, useContext } from "react"
 import Grid from "@material-ui/core/Grid"
 import Typography from "@material-ui/core/Typography"
+import Pagination from "@material-ui/lab/Pagination"
+import PaginationItem from "@material-ui/lab/PaginationItem"
 import { makeStyles } from "@material-ui/core/styles"
 import { useQuery } from "@apollo/client"
 
@@ -13,12 +15,26 @@ const useStyles = makeStyles(theme => ({
   reviews: {
     padding: "0 3rem",
   },
+  pagination: {
+    marginBottom: "3rem",
+  },
+  "@global": {
+    ".MuiPaginationItem-root": {
+      fontFamily: "Montserrat",
+      fontSize: "2rem",
+      color: theme.palette.primary.main,
+      "&.Mui-selected": {
+        color: "#fff",
+      },
+    },
+  },
 }))
 
 export default function ProductReviews({ product, edit, setEdit }) {
   const classes = useStyles()
   const { user } = useContext(UserContext)
   const [reviews, setReviews] = useState([])
+  const [page, setPage] = useState(1)
 
   const { data } = useQuery(GET_REVIEWS, { variables: { id: product } })
 
@@ -27,6 +43,9 @@ export default function ProductReviews({ product, edit, setEdit }) {
       setReviews(data.product.reviews)
     }
   }, [data])
+
+  const reviewsPerPage = 15
+  const numPages = Math.ceil(reviews.length / reviewsPerPage)
 
   return (
     <Grid
@@ -49,6 +68,7 @@ export default function ProductReviews({ product, edit, setEdit }) {
         .filter(review =>
           edit ? review.user.username !== user.username : review
         )
+        .slice((page - 1) * reviewsPerPage, page * reviewsPerPage)
         .map(review => (
           <ProductReview
             reviews={reviews}
@@ -57,6 +77,17 @@ export default function ProductReviews({ product, edit, setEdit }) {
             review={review}
           />
         ))}
+      <Grid item container justify="flex-end">
+        <Grid item>
+          <Pagination
+            classes={{ root: classes.pagination }}
+            count={numPages}
+            page={page}
+            onChange={(e, newPage) => setPage(newPage)}
+            color="primary"
+          />
+        </Grid>
+      </Grid>
     </Grid>
   )
 }
