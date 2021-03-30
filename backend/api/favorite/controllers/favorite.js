45cfc7be426dfcbd5efb7ab901282258234acc58
Zachary Reece <zachdreece@gmail.com>
2021-03-29 23:16:43 -0500

    Favorites Controllers

--- backend/api/favorite/controllers/favorite.js
+++ backend/api/favorite/controllers/favorite.js
@@ -1,8 +1,37 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+const { parseMultipartData, sanitizeEntity } = require("strapi-utils");
+
+module.exports = {
+  async create(ctx) {
+    let entity;
+    if (ctx.is("multipart")) {
+      const { data, files } = parseMultipartData(ctx);
+      data.user = ctx.state.user.id;
+      entity = await strapi.services.favorite.create(data, { files });
+    } else {
+      ctx.request.body.user = ctx.state.user.id;
+      entity = await strapi.services.favorite.create(ctx.request.body);
+    }
+    return sanitizeEntity(entity, { model: strapi.models.favorite });
+  },
+
+  async delete(ctx) {
+    const { id } = ctx.params;
+
+    const [favorite] = await strapi.services.favorite.find({
+      id,
+      user: ctx.state.user.id,
+    });
+
+    if (!favorite) return ctx.unauthorized("You can't update this entry.");
+
+    const entity = await strapi.services.restaurant.delete({ id });
+    return sanitizeEntity(entity, { model: strapi.models.restaurant });
+  },
+};
