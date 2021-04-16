d19fa4d82668e0feccbd21a343f2a0dd6ff2d229
Zachary Reece <zachdreece@gmail.com>
2021-04-16 00:18:00 -0500

    Delete Favorite From List

--- frontend/src/components/settings/Favorites.js
+++ frontend/src/components/settings/Favorites.js
@@ -3,6 +3,7 @@ import axios from "axios"
 import Grid from "@material-ui/core/Grid"
 import Typography from "@material-ui/core/Typography"
 import IconButton from "@material-ui/core/IconButton"
+import CircularProgress from "@material-ui/core/CircularProgress"
 import Chip from "@material-ui/core/Chip"
 import { makeStyles } from "@material-ui/core/styles"
 
@@ -14,7 +15,7 @@ import SettingsGrid from "./SettingsGrid"
 import Delete from "../../images/Delete"
 
 import { UserContext, FeedbackContext } from "../../contexts"
-import { setSnackbar } from "../../contexts/actions"
+import { setSnackbar, setUser } from "../../contexts/actions"
 
 const useStyles = makeStyles(theme => ({
   container: {
@@ -45,7 +46,8 @@ export default function Favorites({ setSelectedSetting }) {
   const [selectedVariants, setSelectedVariants] = useState({})
   const [selectedSizes, setSelectedSizes] = useState({})
   const [selectedColors, setSelectedColors] = useState({})
-  const { user } = useContext(UserContext)
+  const [loading, setLoading] = useState(null)
+  const { user, dispatchUser } = useContext(UserContext)
   const { dispatchFeedback } = useContext(FeedbackContext)
 
   const setSelectedHelper = (selectedFunction, values, value, row) => {
@@ -105,6 +107,44 @@ export default function Favorites({ setSelectedSetting }) {
         id: item.id,
       }
     })
+
+  const handleDelete = row => {
+    setLoading(row)
+
+    axios
+      .delete(process.env.GATSBY_STRAPI_URL + `/favorites/${row}`, {
+        headers: { Authorization: `Bearer ${user.jwt}` },
+      })
+      .then(response => {
+        setLoading(null)
+
+        const newProducts = products.filter(product => product.id !== row)
+        const newFavorites = user.favorites.filter(
+          favorite => favorite.id !== row
+        )
+        setProducts(newProducts)
+        dispatchUser(setUser({ ...user, favorites: newFavorites }))
+
+        dispatchFeedback(
+          setSnackbar({
+            status: "success",
+            message: "Product Removed From Favorites.",
+          })
+        )
+      })
+      .catch(error => {
+        setLoading(null)
+        console.error(error)
+
+        dispatchFeedback(
+          setSnackbar({
+            status: "error",
+            message:
+              "There was a problem removing this product from your favorites. Please try again.",
+          })
+        )
+      })
+  }
 
   const columns = [
     {
@@ -206,11 +246,15 @@ export default function Favorites({ setSelectedSetting }) {
       width: 500,
       sortable: false,
       disableColumnMenu: true,
-      renderCell: ({ value }) => (
-        <IconButton>
-          <span className={classes.deleteWrapper}>
-            <Delete />
-          </span>
+      renderCell: ({ value, row }) => (
+        <IconButton onClick={() => handleDelete(row.id)} disabled={!!loading}>
+          {loading === row.id ? (
+            <CircularProgress size="2rem" color="secondary" />
+          ) : (
+            <span className={classes.deleteWrapper}>
+              <Delete />
+            </span>
+          )}
         </IconButton>
       ),
     },
