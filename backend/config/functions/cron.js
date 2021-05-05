0d009eb8ed5f0ea93ec8abfd526738752738d620
Zachary Reece <zachdreece@gmail.com>
2021-05-04 23:14:19 -0500

    Make Recurring Charges

--- backend/config/functions/cron.js
+++ backend/config/functions/cron.js
@@ -10,6 +10,8 @@
  * See more details here: https://strapi.io/documentation/v3.x/concepts/configurations.html#cron-tasks
  */
 
+const stripe = require("stripe")(process.env.STRIPE_SK);
+
 module.exports = {
   /**
    * Simple example.
@@ -18,10 +20,36 @@ module.exports = {
   // '0 1 * * 1': () => {
   //
   // }
-  // "0 8 * * *": async () => {
-  //
-  // }
-  "*/1 * * * * *": async (date) => {
-    console.log(date);
+  "0 8 * * *": async () => {
+    const subscriptions = await strapi.services.subscription.find({
+      next_delivery: new Date(),
+    });
+
+    await Promise.allSettled(
+      subscriptions.map(async (subscription) => {
+        const paymentMethods = await stripe.paymentMethods.list({
+          customer: subscription.user.stripeID,
+          type: "card",
+        });
+
+        const paymentMethod = paymentMethods.data.find(
+          (method) => method.card.last4 === subscription.paymentMethod.last4
+        );
+
+        try {
+          const paymentIntent = await stripe.paymentIntents.create({
+            amount: Math.round(subscription.variant.price * 1.075 * 100),
+            currency: "usd",
+            customer: subscription.user.stripeID,
+            payment_method: paymentMethod.id,
+            off_session: true,
+            confirm: true,
+          });
+        } catch (error) {
+          //Notify customer payment failed, and prompt them to enter new information
+          console.log(error);
+        }
+      })
+    );
   },
 };
