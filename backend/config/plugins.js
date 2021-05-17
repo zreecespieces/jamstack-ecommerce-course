4c26aa26fd571eba7fbb2a606aceabeb1300f320
Zachary Reece <zachdreece@gmail.com>
2021-05-17 01:10:24 -0500

    Clone Project To EC2

--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -9,4 +9,15 @@ module.exports = ({ env }) => ({
       defaultTo: "jamstack.course@gmail.com",
     },
   },
+  upload: {
+    provider: "aws-s3",
+    providerOptions: {
+      accessKeyId: env("AWS_ACCESS_KEY_ID"),
+      secretAccessKey: env("AWS_ACCESS_SECRET"),
+      region: env("AWS_REGION"),
+      params: {
+        Bucket: env("AWS_BUCKET_NAME"),
+      },
+    },
+  },
 });
