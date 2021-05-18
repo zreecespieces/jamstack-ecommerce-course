bbf3fb960031e850d90f2dd21d9ca06d9b590cb9
Zachary Reece <zachdreece@gmail.com>
2021-05-17 23:44:59 -0500

    Custom Domain With NGINX

--- backend/config/server.js
+++ backend/config/server.js
@@ -2,6 +2,7 @@ module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
   cron: { enabled: true },
+  url: env("URL", "http://localhost"),
   admin: {
     auth: {
       secret: env("ADMIN_JWT_SECRET", "c423fcb4b1d5ec19947db8a7a7bb9104"),
