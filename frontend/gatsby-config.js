8ba9343fdedb8611e442cd9e5b425cfad3f22895
Zachary Reece <zachdreece@gmail.com>
2021-05-31 23:03:28 -0500

    Gatsby SEO Plugins

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -24,6 +24,15 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-material-ui`,
+    `gatsby-plugin-sitemap`,
+    {
+      resolve: `gatsby-plugin-robots-txt`,
+      options: {
+        host: "https://formstorm.design",
+        sitemap: "https://formstorm.design/sitemap.xml",
+        policy: [{ userAgent: "*", allow: "/" }],
+      },
+    },
     {
       resolve: "gatsby-plugin-web-font-loader",
       options: {
@@ -52,20 +61,18 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    // {
-    //   resolve: `gatsby-plugin-manifest`,
-    //   options: {
-    //     name: `gatsby-starter-default`,
-    //     short_name: `starter`,
-    //     start_url: `/`,
-    //     background_color: `#663399`,
-    //     theme_color: `#663399`,
-    //     display: `minimal-ui`,
-    //     // icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-    //   },
-    // },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `VAR-X`,
+        short_name: `VAR-X`,
+        start_url: `/`,
+        background_color: `#99B898`,
+        theme_color: `#99B898`,
+        display: `minimal-ui`,
+        icon: `src/images/favicon.png`,
+      },
+    },
+    `gatsby-plugin-offline`,
   ],
 }
